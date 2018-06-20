16538c0e4dc4d0dbd10a0654d9335368768afa97
Daria <dashytc@yanddex.ru>
2018-06-20 12:19:44 +0300

    Доработка программы (урок 3)

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,6 @@
 # coding : utf-8
+import os
+import psutil      #сторонний модуль
 
 print("Мастер Питона :-)")
 print("привет, программист!")
@@ -9,13 +11,22 @@ print(name, ", добро пожаловать!")
 answer = input("Давайте поработаем?  (Y/N)")
 
 if answer == "Y":
-	doing = input("Вы хотите кодировать или писать требования? (C/R)")		#pass #print("Вам премия!")
-	if doing == "C":
-		print("Откройте свой файл в блокноте")
-	elif doing == "R":
-		print("Откройте вордовский файл")
+	print("Отлично, хозяин!")
+	print("Я умею:")
+	print("[1] - выведу список файлов")
+	print("[2] - выведу информацию о системе")
+	print("[3] - выведу список процессов")
+	do = int(input("Укажите номер действия: "))
+
+	if do == 1:
+		print(os.listdir())
+	elif do == 2:
+		pass
+	elif do == 3:
+		print(psutil.pids())
 	else:
-		print("Неизвестный ответ")
+		pass
+
 elif answer == "N":
 	print("До свидания!")
 else:
