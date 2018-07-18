720818648d193697d3cc2941fbdafc9788db79c1
Daria <dashytc@yanddex.ru>
2018-07-18 15:51:50 +0300

    Русская рулетка с наказанием от робота (дублирование файлов)

---
+++ mrrobot.py
@@ -0,0 +1,99 @@
+# coding : utf-8
+import os
+import psutil      #сторонний модуль
+import sys
+import shutil
+
+def duplicate_file(filename):
+	if os.path.isfile(filename):
+				newfile = filename + ".dupl"
+				shutil.copy(filename, newfile)
+				if os.path.exists(newfile):
+					print("Файл ", newfile, " был успешно создан")
+					return True
+				else:
+					print("Возникли проблемы при копировании")
+					return False
+
+def duble_files(dirname):
+	file_list = os.listdir(dirname)
+	i = 1
+	while i < len(file_list):
+		duplicate_file(file_list[i])
+		i += 1
+
+def sys_info():
+	print("Текущая рабочая директория: ", os.getcwd())
+	print( "Платформа (ОС): ", sys.platform)
+	print( "Кодировка файловой системы: ", sys.getfilesystemencoding())
+	print( "Логин пользователя: ", os.getlogin())
+	print( "Количество CPU:", psutil.cpu_count())
+
+def del_dubl(direct):
+	file_list = os.listdir(direct)
+	double_count = 0
+	for f in file_list:
+		fullname = os.path.join(direct, f)
+		if  fullname.endswith(".dupl"):
+			os.remove(fullname)
+			if not os.path.exists(fullname):
+					print("Файл ", fullname, " был успешно создан")
+					double_count += 1
+				else:
+					print("Возникли проблемы при копировании")
+	return double_count
+
+def main():
+	print("Мастер Питона :-)")
+	print("привет, программист!")
+	name = input("Ваше имя: ")
+
+	print(name, ", добро пожаловать!")
+
+	answer = ""
+
+	while answer != 'q':
+		answer = input("Давайте поработаем?  (Y/N/q) ")
+
+		if answer == "Y":
+			print("Отлично, хозяин!")
+			print("Я умею:")
+			print("[1] - выведу список файлов")
+			print("[2] - выведу информацию о системе")
+			print("[3] - выведу список процессов")
+			print("[4] - продублирую  файлы в текущей директории")
+			print("[5] - дублирование указанного файла")
+			print("[6] - удаление файлов с окончанием dupl в текущей директории")
+			do = int(input("Укажите номер действия: "))
+
+			if do == 1:
+				print(os.listdir())
+		
+			elif do == 2:
+				sys_info()
+
+			elif do == 3:
+				print(psutil.pids())
+		
+			elif do == 4:
+				print("---Дублирование файлов в текущей директории")
+				duble_files('.')
+		
+			elif do == 5:
+				file_dupl = input("Какой файл дублировать? ")
+				duplicate_file(file_dupl)
+
+			elif do == 6:
+				direct = input("Укажите директорию ")
+				count = str(del_dubl(direct))
+				print("Удалено " + count + " файлов")
+			else:
+				pass
+
+		elif answer == "N":
+			print("До свидания!")
+		else:
+			print("Неизвестный выбор")
+
+if _name_ == "_main_":
+	main()
---
+++ russian_rulet.py
@@ -0,0 +1,63 @@
+# coding : utf-8
+
+import turtle
+import random
+import math
+
+import mrrobot
+
+PHI = 360/7
+R = 50
+
+def gotoxy(x, y):
+	turtle.penup()
+	turtle.goto(x, y)
+	turtle.pendown()
+
+def draw_circle(r, color):
+	turtle.fillcolor(color)
+	turtle.begin_fill()
+	turtle.circle(r)
+	turtle.end_fill()
+
+def draw_drum(x0, y0):
+	#основной круг
+	gotoxy(x0,y0)
+	turtle.circle(80)  #барабан
+	#мушка
+	gotoxy(x0,160 + y0)
+	draw_circle(5, "red")
+	#барабан
+	for i in range(0,7):
+		phi_rad = PHI * i * math.pi / 180.0
+		gotoxy(x0 + math.sin(phi_rad) * R, y0 + math.cos(phi_rad) * R + 60)
+		draw_circle(22, "white")
+
+def draw_rot(x0, y0, start):
+	for i in range(start,random.randrange(7,100)):
+		phi_rad = PHI * i * math.pi / 180.0
+		gotoxy(x0 + math.sin(phi_rad) * R, y0 + math.cos(phi_rad) * R + 60)
+		draw_circle(22, "brown")
+		draw_circle(22, "white")
+	gotoxy(x0 + math.sin(phi_rad) * R, y0 + math.cos(phi_rad) * R + 60)
+	draw_circle(22, "brown")
+	return i % 7
+
+turtle.speed(0) 
+draw_drum(0, 0)
+
+answer = ''
+start = 0
+
+while answer != 'N':
+	answer = turtle.textinput("Играть?", "Y/N")
+	if answer == 'Y':
+		#пуля
+		start = draw_rot(0, 0, start)
+	
+		if start == 0:
+			gotoxy(-150, 250)
+			turtle.write("Вы проиграли!", font=("Arial", 18, "normal"))
+			mrrobot.duble_files('.')
+	else:
+		pass
